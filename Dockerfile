ca92852cbe1f320766ba6a3712645c699fa140c5
Garrett Koller <gkoller@catalist.us>
2021-02-16 02:07:09 -0500

    v2.0.0 Docker image entrypoint is now `git-lfs`
    instead of undefined

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM alpine:latest
 RUN apk update && apk add git-lfs && git lfs install
+ENTRYPOINT git-lfs
--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,4 +1,34 @@
 #!/usr/bin/env bash
+# Usage: buildAndPush.sh <new_version_number>
+# Example: buildAndPush.sh 1.0.0
 
-docker build --tag git-lfs:latest --tag gcr.io/public-side-projects/git-lfs:latest .
-docker push gcr.io/public-side-projects/git-lfs:latest
+if [ $# -ne 1 ]; then
+	echo 'Usage: buildAndPush.sh <new_version_number>'
+	exit 1
+fi
+
+ver="$1"
+
+main() {
+	docker build --tag git-lfs:latest \
+	             --tag git-lfs:"$ver" \
+	             --tag gcr.io/public-side-projects/git-lfs:latest \
+	             --tag gcr.io/public-side-projects/git-lfs:"$ver" \
+	             . \
+		|| exitOnError 'docker build failed'
+	docker push gcr.io/public-side-projects/git-lfs:latest \
+		|| exitOnError 'docker push latest failed'
+	docker push gcr.io/public-side-projects/git-lfs:"$ver" \
+		|| exitOnError "docker push $ver failed"
+}
+
+exitOnError() {
+	if [ -n "$1" ]; then
+		echo -e "<== ERROR: $1 \a\a\a"
+	else
+		echo -e '<== An error occured. Exiting just in case. \a\a\a'
+	fi
+	exit 2
+}
+
+main
